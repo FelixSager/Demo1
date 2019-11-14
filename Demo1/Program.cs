e6eaa929aba2904c260372867b2b493c248619c0
Tobias Ansey <tobias.ansey@outlook.com>
2019-11-14 13:37:49 +0100

    First commit

---
+++ Demo1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Gtec.Unicorn;
+
+namespace UnicornNetAcquisitionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("API Version: " + Unicorn.GetApiVersion());
+            Console.ReadLine();
+        }
+    }
+}
